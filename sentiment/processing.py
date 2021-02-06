071d49ee64acf219f4d7a871a1ee82f5958b83ce
Yonatan Gottesman <yonigo10@gmail.com>
2021-02-06 22:17:40 +0200

    revert model version

--- sentiment/processing.py
+++ sentiment/processing.py
@@ -4,8 +4,4 @@ from nltk.tokenize import TweetTokenizer
 
 
 def tokenize(tweet: str) -> List[str]:
-    tokenizer = TweetTokenizer(preserve_case=False,
-                               strip_handles=True,
-                               reduce_len=True)
-    tweet_tokens = tokenizer.tokenize(tweet)
-    return tweet_tokens
+    return tweet.split()
