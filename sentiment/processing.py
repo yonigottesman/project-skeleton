81c9b07a2a1f370da35408f0a1cfc4aa150228cc
Yonatan Gottesman <yonigo10@gmail.com>
2021-02-07 14:13:56 +0200

    better tokenizer

--- sentiment/processing.py
+++ sentiment/processing.py
@@ -2,4 +2,8 @@ from typing import List
 
 
 def tokenize(tweet: str) -> List[str]:
-    return tweet.split()
+    tokenizer = TweetTokenizer(preserve_case=False,
+                               strip_handles=True,
+                               reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+    return tweet_tokens
