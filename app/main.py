733bc60c5241ff36582327dc6262cd2420efba93
Yonatan Gottesman <yonigo10@gmail.com>
2021-02-07 10:08:07 +0200

    First try to deploy on eb

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from sentiment import MODEL_VERSION
 from sentiment.inference import SentimentInference
 
 app = FastAPI()
@@ -11,6 +12,7 @@ inferencer = SentimentInference()
 def read_item(tweet: str):
     prob = inferencer.inference(tweet)
     return {
+        'version': MODEL_VERSION,
         'tweet': tweet,
         'sentiment': 'positive' if prob == 1 else 'negative'
     }
